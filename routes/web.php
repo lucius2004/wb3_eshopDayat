838151d0be5bc95c2d2420a83fc1b1d6f1e6d8d4
dayat <akuntumbal4902@gmail.com>
2025-03-24 21:39:47 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ProdukController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return redirect()->route('backend.login');
+});
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->name('backend.beranda');
+Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
+Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login.authenticate');
+Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
+
+// Route::resource('backend/user', UserController::class)->middleware('auth');
+Route::resource('/backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
+// Route::resource('backend/user', UserController::class)->middleware('auth');
+
+Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth');
+Route::resource('backend/produk', ProdukController::class, ['as' => 'backend'])->middleware('auth');
+// Route untuk menambahkan foto
+Route::post('foto-produk/store', [ProdukController::class, 'storeFoto'])->name('backend.foto_produk.store')->middleware('auth');
+// Route untuk menghapus foto
+Route::delete('foto-produk/{id}', [ProdukController::class, 'destroyFoto'])->name('backend.foto_produk.destroy')->middleware('auth');
+Route::get('backend/laporan/formuser', [UserController::class, 'formUser'])->name('backend.laporan.formuser')->middleware('auth');
+Route::post('backend/laporan/cetakuser', [UserController::class, 'cetakUser'])->name('backend.laporan.cetakuser')->middleware('auth');
+
+Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk'])->name('backend.laporan.formproduk')->middleware('auth');
+Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])->name('backend.laporan.cetakproduk')->middleware('auth');
+
